e35eb3207b3f59ddb5a6bb697c20c045e3284a80
Samuel Raducan <samuel.raducann@gmail.com>
2021-02-07 15:27:55 +0000

    Modal closed when a user clicks outside.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 /* NOTE: Global Variables */
-const modalButton = document.querySelector('.jsModalButton');
-const modalCloseButton = document.querySelector('.jsModalClose');
+const modalButton = document.querySelector('.button');
+const modalCloseButton = document.querySelector('.modal__close-button');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
 
 /* NOTE: Event Listeners */
+
+// Open the modal
 modalButton.addEventListener('click', event => {
   console.log('Button was clicked.');
   document.body.classList.add('modal-is-open');
 });
 
+// Close the modal
 modalCloseButton.addEventListener('click', event => {
   console.log('You pressed the icon x');
   document.body.classList.remove('modal-is-open');
+});
+
+// Close the modal when clicked outside the modal
+modalOverlay.addEventListener('click', event => {
+  console.log(event.target);
+  if (!event.target.closest('.modal')) {
+    document.body.classList.remove('modal-is-open');
+  }
 });
