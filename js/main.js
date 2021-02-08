ec1b683c3fcc8a79d07deabcde68b48b3cec51d9
Samuel Raducan <samuel.raducann@gmail.com>
2021-02-08 12:36:10 +0000

    Added animation with GSAP  to the waving hand.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,30 @@
 const modalButton = document.querySelector('.button');
 const modalCloseButton = document.querySelector('.modal__close-button');
 const modalOverlay = document.querySelector('.modal-overlay');
-const modal = document.querySelector('.modal');
+const wavingHand = document.querySelector('.wave-hand');
+
+/* NOTE: Functions */
+const wave = hand => {
+  const tl = new TimelineMax({});
+  tl.set(hand, { transformOrigin: 'bottom center' });
+  tl.from(hand, 0.5, {
+    scale: 0.25,
+    opacity: 0,
+    ease: Back.easeOut.config(1.5),
+  });
+  tl.to(hand, 0.2, { rotation: 15 });
+  tl.to(hand, 0.2, { rotation: -15 });
+  tl.to(hand, 0.2, { rotation: 15 });
+  tl.to(hand, 0.2, { rotation: -15 });
+  tl.to(hand, 0.2, { rotation: 0 });
+};
 
 /* NOTE: Event Listeners */
 
 // Open the modal
 modalButton.addEventListener('click', event => {
-  console.log('Button was clicked.');
   document.body.classList.add('modal-is-open');
+  wave(wavingHand);
 });
 
 // Close the modal
